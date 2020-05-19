c8220199548cb359b15d6eccfef63cdc57da4fa1
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-05-18 20:32:56 -0400

    heroku stuff added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const server = express();
+require('dotenv').config();
+
 const postsRouter = require('./postsRouter.js');
 
 
@@ -8,11 +10,13 @@ server.use(express.json());
 
 server.use('/api/posts', postsRouter);
 
+const port = process.env.PORT || 5000;
+
 
 server.use('/', (req, res) => {
     res.status(200).send("hello from index.js server");
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('The server is running');
 });
